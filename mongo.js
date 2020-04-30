b789317e320f0c14f01d01ac6a1e731b9c499442
Vivekanandan <vivekanandansakthivelu@gmail.com>
2020-04-30 13:32:58 +0530

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -2,4 +2,4 @@
 db.collections.find({ "generation":"17CY" });
 
 # find not  equal
-db.collections.find({generation:{$ne:"17CYPLUYS"}});
+db.collections.find({ "generation" : { $ne : "17CYPLUYS" } });
